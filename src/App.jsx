9e2c6abb721ad53bcebeb5c793ef385973d31133
Anita Virag <ik.virag@gmail.com>
2019-02-11 23:18:38 +0100

    add todo, delete todo

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Todos from './Todos';
+import AddTodo from './components/AddTodo';
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'play mario cart' }
+    ]
+  };
+
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => todo.id !== id);
+    this.setState({todos});
+  }
+
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [
+      ...this.state.todos,
+      todo
+    ];
+
+    this.setState({todos});
+  }
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
